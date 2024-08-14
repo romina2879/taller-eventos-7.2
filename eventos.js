5f537ee82d0dab2f96bd6a42331c0f1c6556683e
romina2879 <ro235.g1@gmail.com>
2024-08-14 10:56:31 -0300

    Creo el .js y el .css

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+const divButton= document.getElementById("contenedor_button");
+
+divButton.addEventListener("click", function() {
+    alert("Hola! Soy el div");
+});
+});
